472197162933c03da0cdc74007e7ac69fcefcf46
MichalKuta82 <43048641+MichalKuta82@users.noreply.github.com>
2018-10-28 10:59:20 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
 //define db params
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "root");
-define("DB_NAME", "shareboard");
+define("DB_HOST", "");
+define("DB_USER", "");
+define("DB_PASS", "");
+define("DB_NAME", "");
 
 //define URL
 define("ROOT_PATH", "/");
